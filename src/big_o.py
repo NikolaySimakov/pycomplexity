43a7d4bc9728e94c4d5e4f99d3b413b98e83a74e
Nikolay Simakov <n.simakoff@gmail.com>
2023-06-29 06:48:38 +0300

    Remove files

--- src/big_o.py
+++
@@ -1,7 +0,0 @@
-class BigO():
-
-    def __init__(self) -> None:
-        pass
-    
-    def complexity(self):
-        pass
--- src/big_omega.py
+++
@@ -1,7 +0,0 @@
-class BigOmega:
-    
-    def __init__(self) -> None:
-        pass
-    
-    def complexity(self):
-        pass
